00e1c239f30637bbdd59cb4f558edffc413ae825
David.Gao <david.alpha.fox@gmail.com>
2016-05-09 12:48:07 +0800

    fix bug in payment_operation.ex and upgrade riakc_common

--- lib/api/payment_operation.ex
+++ lib/api/payment_operation.ex
@@ -6,12 +6,11 @@ defmodule PayPal.Api.PaymentOperation do
   alias RiakcCommon.SimpleRest.Utils.API
   alias PayPal.Objects.PaymentResponse
   
-  def execuate(target,token,invoice_id,payer_id) do
-    header = [{"Authorization", "Bearer " <>  token}]
+  def execuate(target,header,invoice_id,payer_id) do
     url = target <> "payments/payment/#{invoice_id}/execute"
     context = api_context(url,PaymentResponse,header)
     data = Poison.encode!(%{payer_id: payer_id})
-    API.post(context.target,data,&context.handler/1,context.headers)
+    API.post(context.target,data,context.handler,context.headers)
   end
 
 
