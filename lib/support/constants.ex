2908bd77c810d5db4214e05e836036ca96ac47c0
David.Gao <david.alpha.fox@gmail.com>
2016-05-11 11:00:29 +0800

    添加常量

---
+++ lib/support/constants.ex
@@ -0,0 +1,24 @@
+defmodule PayPal.Support.Constants do
+  use RiakcCommon.Tools.Constants
+
+  define intent_sale "sale"
+  define intent_authorize "authorize"
+  define intent_order "order"
+
+  define state_created "created"
+  define state_approved "approved"
+  define state_failed "failed"
+  define state_canceled "canceled"
+  define state_expired "expired"
+  define state_pending "pending"
+  define state_in_progress "in_progress"
+  
+
+  define event_sale_completed "PAYMENT.SALE.COMPLETED"
+  define event_sale_denied "PAYMENT.SALE.DENIED"
+  define event_sale_pending "PAYMENT.SALE.PENDING"
+  define event_sale_refunded "PAYMENT.SALE.REFUNDED"
+  define event_sale_reversed "PAYMENT.SALE.REVERSED"
+
+
+end
